d1ced3306e1362597e201957ed6b5ac81632a784
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2023-03-18 21:33:07 +0100

    Flood distance calculation

--- g5/src/main/java/dtu/aimas/common/Position.java
+++ g5/src/main/java/dtu/aimas/common/Position.java
@@ -12,4 +12,22 @@ public class Position {
         this.row = row;
         this.col = col;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if(!(o instanceof Position)) return false;
+        Position c = (Position) o;
+        return c.row == this.row && c.col == this.col;
+    }
+
+    @Override
+    public int hashCode() {
+        //Necessary to use hashed data structures
+        //inspired by the javafx.util.Pair hashCode function
+        int hash = 7;
+        hash = 31 * hash + Integer.valueOf(row).hashCode();
+        hash = 31 * hash + Integer.valueOf(col).hashCode();
+        return hash;
+    }
 }
--- g5/src/main/java/dtu/aimas/search/Problem.java
+++ g5/src/main/java/dtu/aimas/search/Problem.java
@@ -1,7 +1,14 @@
 package dtu.aimas.search;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.stream.Collectors;
+import java.util.Queue;
+import java.util.Set;
+import java.util.LinkedList;
+import dtu.aimas.common.Position;
+import dtu.aimas.communication.IO;
 import dtu.aimas.common.Agent;
 import dtu.aimas.common.Box;
 
@@ -10,7 +17,8 @@ public class Problem {
     public Collection<Agent> agents;
     public Collection<Box> boxes;
     public boolean[][] walls;
-    public char[][]goals;
+    public char[][] goals;
+    public int[][][][] distances;
 
     public Problem(Collection<Agent> agentCollection, Collection<Box> boxCollection, boolean[][] walls, char[][] goals) 
     {
@@ -18,9 +26,59 @@ public class Problem {
         this.boxes = boxCollection;
         this.walls = walls;
         this.goals = goals;
+        this.distances = new int[walls.length][walls[0].length][walls.length][walls[0].length];
+        for(int i = 0; i < distances.length; i++) {
+            for(int j = 0; j < distances[i].length; j++) {
+                for(int l = 0; l < distances[i][j].length; l++) {
+                    Arrays.fill(distances[i][j][l], Integer.MAX_VALUE);
+                }
+            }
+        }
+    }
+
+    private void distanceFromPos(Position src) {
+        Queue<Position> neighbors = new LinkedList<Position>();
+        Set<Position> visited = new HashSet<Position>();
+        visited.add(src);
+        distances[src.row][src.col][src.row][src.col] = 0;
+        neighbors.add(src);
+        while(!neighbors.isEmpty()) {
+            Position curr = neighbors.remove();
+            int distToCurr = distances[src.row][src.col][curr.row][curr.col];
+            
+            Position top = new Position(curr.row-1,curr.col);
+            if(top.row >= 0 && !visited.contains(top) && !walls[top.row][top.col]) {
+                visited.add(top);
+                distances[src.row][src.col][top.row][top.col] = distToCurr+1;
+                neighbors.add(top);             
+            }
+            Position left = new Position(curr.row,curr.col-1);
+            if (left.col >= 0 && !visited.contains(left) && !walls[left.row][left.col]) {
+                visited.add(left);
+                distances[src.row][src.col][left.row][left.col] = distToCurr+1;
+                neighbors.add(left);
+            }
+            Position bot = new Position(curr.row+1,curr.col);
+            if (bot.row < walls.length && !visited.contains(bot) && !walls[bot.row][bot.col]) {
+                visited.add(bot);
+                distances[src.row][src.col][bot.row][bot.col] = distToCurr+1;
+                neighbors.add(bot);
+            }
+            Position right = new Position(curr.row,curr.col+1);
+            if (right.col < walls[right.row].length && !visited.contains(right) && !walls[right.row][right.col]) {
+                visited.add(right);
+                distances[src.row][src.col][right.row][right.col] = distToCurr+1;
+                neighbors.add(right);
+            }
+        }
     }
 
     public Problem precompute(){
+        for(int x = 0; x < walls.length; x++) {
+            for(int y = 0; y < walls[x].length; y++) {
+                distanceFromPos(new Position(x,y));
+            }
+        }
         return this;
     }
 
