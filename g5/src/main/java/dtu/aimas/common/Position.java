d37889604937c25990810af13e03cbc3c3f8fc5c
martinkarlik <kar@captureone.com>
2023-03-22 10:05:10 +0100

    Resolve remaining merge problems to compile the code.

--- g5/src/main/java/dtu/aimas/common/Position.java
+++ g5/src/main/java/dtu/aimas/common/Position.java
@@ -16,15 +16,6 @@ public class Position {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Position)) return false;
-        Position position = (Position) o;
-        return row == position.row &&
-                col == position.col;
-    }
-
-    @Override
     public int hashCode() {
         return Objects.hash(row, col);
     }
@@ -37,13 +28,4 @@ public class Position {
         return c.row == this.row && c.col == this.col;
     }
 
-    @Override
-    public int hashCode() {
-        //Necessary to use hashed data structures
-        //inspired by the javafx.util.Pair hashCode function
-        int hash = 7;
-        hash = 31 * hash + Integer.valueOf(row).hashCode();
-        hash = 31 * hash + Integer.valueOf(col).hashCode();
-        return hash;
-    }
 }
