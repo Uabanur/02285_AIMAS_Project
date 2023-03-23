d15d72fd79b110f873230f1e2b754bf8bcdb3653
= <=>
2023-03-23 21:07:14 +0100

    runs server gui

--- run.bat
+++ run.bat
@@ -3,5 +3,6 @@ set levelDir=.\g5\src\main\resources\levels
 set classesDir=.\g5\target\classes
 set clientName=dtu.aimas.SearchClient
 set arguments=-bfs -log debug servermessages
+set flags=-g
 
-java -jar .\mavis.jar -l "%levelDir%\SAD1.lvl" -c "java -cp %classesDir% %clientName% %arguments%"
+java -jar .\mavis.jar -l "%levelDir%\SAD1.lvl" -c "java -cp %classesDir% %clientName% %arguments%" %flags%
