1aa9a62b04575bb5d9b38c9a233375ca82c959ab
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2023-04-17 17:43:45 +0200

    More tests

--- g5/src/test/java/dtu/aimas/SubProblem2Test.java
+++ g5/src/test/java/dtu/aimas/SubProblem2Test.java
@@ -189,5 +189,87 @@ public class SubProblem2Test {
         Assert.assertArrayEquals(expectedBoxGoals0, subProblem0.boxGoals.toArray(Goal[]::new));
         Assert.assertArrayEquals(expectedAgentGoals0, subProblem0.agentGoals.toArray(Goal[]::new));
     }
+
+    @Test
+    public void SubProblem_Of_Optimal_Box(){
+        var height = 10;
+        var width = 10;
+
+        var agents = List.of(
+            new Agent(new Position(0, 0), Color.Red, '0')
+        );
+
+        var boxes = List.of(
+            new Box(new Position(1, 9), Color.Blue, 'A'),
+            new Box(new Position(2, 0), Color.Red, 'A'),
+            new Box(new Position(0, 7), Color.Red, 'A')
+        );
+
+        var walls = new boolean[width][height];
+        var goals = new char[width][height];
+        
+        goals[5][5] = agents.get(0).label;
+        goals[0][9] = 'A';
+
+        var problem = new Problem(agents, boxes, walls, goals);
+        problem.assignGoals();
+
+        var subProblem0 = problem.subProblemFor2(agents.get(0));
+        var expectedAgents0 = new Agent[]{agents.get(0)};
+        var expectedBoxes0 = new Box[]{boxes.get(2)};
+        var expectedBoxGoals0 = new Goal[]{new Goal('A', new Position(0,9))};
+        var expectedAgentGoals0 = new Goal[]{};
+        Assert.assertArrayEquals(expectedAgents0, subProblem0.agents.toArray(Agent[]::new));
+        Assert.assertArrayEquals(expectedBoxes0, subProblem0.boxes.toArray(Box[]::new));
+        Assert.assertArrayEquals(expectedBoxGoals0, subProblem0.boxGoals.toArray(Goal[]::new));
+        Assert.assertArrayEquals(expectedAgentGoals0, subProblem0.agentGoals.toArray(Goal[]::new));
+    }
+
+    @Test
+    public void SubProblem_Fight_For_BoxGoal(){
+        var height = 10;
+        var width = 10;
+
+        var agents = List.of(
+            new Agent(new Position(0, 0), Color.Blue, '0'),
+            new Agent(new Position(5, 5), Color.Red, '1')
+        );
+
+        var boxes = List.of(
+            new Box(new Position(0, 5), Color.Blue, 'A'),
+            new Box(new Position(2, 0), Color.Red, 'A'),
+            new Box(new Position(1, 9), Color.Green, 'A')
+        );
+
+        var walls = new boolean[width][height];
+        var goals = new char[width][height];
+        
+        goals[5][5] = agents.get(0).label;
+        goals[9][9] = agents.get(1).label;
+        goals[0][9] = 'A';
+
+        var problem = new Problem(agents, boxes, walls, goals);
+        problem.assignGoals();
+
+        var subProblem0 = problem.subProblemFor2(agents.get(0));
+        var expectedAgents0 = new Agent[]{agents.get(0)};
+        var expectedBoxes0 = new Box[]{boxes.get(0)};
+        var expectedBoxGoals0 = new Goal[]{new Goal('A', new Position(0,9))};
+        var expectedAgentGoals0 = new Goal[]{};
+        Assert.assertArrayEquals(expectedAgents0, subProblem0.agents.toArray(Agent[]::new));
+        Assert.assertArrayEquals(expectedBoxes0, subProblem0.boxes.toArray(Box[]::new));
+        Assert.assertArrayEquals(expectedBoxGoals0, subProblem0.boxGoals.toArray(Goal[]::new));
+        Assert.assertArrayEquals(expectedAgentGoals0, subProblem0.agentGoals.toArray(Goal[]::new));
+
+        var subProblem1 = problem.subProblemFor2(agents.get(1));
+        var expectedAgents1 = new Agent[]{agents.get(1)};
+        var expectedBoxes1 = new Box[]{};
+        var expectedBoxGoals1 = new Goal[]{};
+        var expectedAgentGoals1 = new Goal[]{new Goal(agents.get(1).label, new Position(9,9))};
+        Assert.assertArrayEquals(expectedAgents0, subProblem0.agents.toArray(Agent[]::new));
+        Assert.assertArrayEquals(expectedBoxes0, subProblem0.boxes.toArray(Box[]::new));
+        Assert.assertArrayEquals(expectedBoxGoals0, subProblem0.boxGoals.toArray(Goal[]::new));
+        Assert.assertArrayEquals(expectedAgentGoals0, subProblem0.agentGoals.toArray(Goal[]::new));
+    }
     
 }
