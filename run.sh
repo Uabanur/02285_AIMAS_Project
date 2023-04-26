454bc0761db1c35df17a632aac2ee101dfd8e4df
MSalwowski <maciej.salwowski@wp.pl>
2023-04-26 23:11:06 +0200

    debugflag added to run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ classesDir="./g5/target/classes"
 clientName="dtu.aimas.SearchClient"
 arguments="-cbs"
 flags="-g"
+debugFlag="-agentlib:jdwp=transport=dt_socket,server=y,address=8000"
 
 java -jar ./mavis.jar -l "$levelDir/FollowPushConflict.lvl" -c "java -cp $classesDir $clientName $arguments" $flags
