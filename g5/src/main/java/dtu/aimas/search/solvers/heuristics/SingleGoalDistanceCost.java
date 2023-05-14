7e525d5a7937b886fdf34f3790c71e023661dc52
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2023-05-14 19:36:00 +0200

    remove old distance computation

--- g5/src/main/java/dtu/aimas/search/solvers/heuristics/SingleGoalDistanceCost.java
+++ g5/src/main/java/dtu/aimas/search/solvers/heuristics/SingleGoalDistanceCost.java
@@ -33,11 +33,11 @@ public class SingleGoalDistanceCost implements Cost {
                 }
             }
             result += minDist;
-            //focus on keeping boxes in goals
+            //distance to box being solved
             if(minDist > 0) {
                 Box b = closestBox;
                 var agent = state.agents.stream().filter(a -> a.color == b.color).findFirst().get();
-                result += problem.admissibleDist(agent.pos, closestBox.pos);
+                result += problem.admissibleDist(agent.pos, closestBox.pos)-1;
             }
             
         }
@@ -51,57 +51,5 @@ public class SingleGoalDistanceCost implements Cost {
         }
 
         return result;
-    }
-
-    private int actualDist(Problem problem, State s, Position from, Position to) {
-        List<Position> goals = List.of(to);
-        if(goals.contains(from)) return 0;       
-        HashMap<Position,Integer> distanceTo = new HashMap<>();
-        LinkedList<Position> neighbors = new LinkedList<>();
-        HashSet<Position> visited = new HashSet<>();
-        neighbors.add(from);
-        visited.add(from);
-        distanceTo.put(from,0);
-        while (!neighbors.isEmpty()) {
-            Position current = neighbors.remove();
-            if (goals.contains(current)) return distanceTo.get(current);
-            int row = current.row;
-            int col = current.col;
-            Position top = new Position(row-1,col);
-            if (row > 0 && !visited.contains(top) && !problem.walls[top.row][top.col]) {
-                visited.add(top);
-                int weight = getWeightPosition(top, s);
-                distanceTo.put(top,distanceTo.get(current)+weight);
-                neighbors.add(top);
-            }
-            Position left = new Position(row,col-1);
-            if (col > 0 && !visited.contains(left) && !problem.walls[left.row][left.col]) {
-                visited.add(left);
-                int weight = getWeightPosition(left, s);
-                distanceTo.put(left,distanceTo.get(current)+weight);
-                neighbors.add(left);
-            }
-            Position bot = new Position(row+1,col);
-            if (row < problem.walls.length - 1 && !visited.contains(bot) && !problem.walls[bot.row][bot.col]) {
-                visited.add(bot);
-                int weight = getWeightPosition(bot, s);
-                distanceTo.put(bot,distanceTo.get(current)+weight);
-                neighbors.add(bot);
-            }
-            Position right = new Position(row,col+1);
-            if (col < problem.walls[row].length - 1 && !visited.contains(right) && !problem.walls[right.row][right.col]) {
-                visited.add(right);
-                int weight = getWeightPosition(right, s);
-                distanceTo.put(right,distanceTo.get(current)+weight);
-                neighbors.add(right);
-            }
-        }
-        return 9999;
-    }
-
-    private static int getWeightPosition(Position pos, State s) {
-        final int BOX_WEIGHT = 10; //could be influenced by map size (to account for time to move box out of blocked place)
-        if (s.boxes.stream().anyMatch(b -> b.pos.equals(pos))) return BOX_WEIGHT;
-        return 1;
     }
 }
