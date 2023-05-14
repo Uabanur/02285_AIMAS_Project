d094960e38c13f9a1259c255fcff5bc3113ece91
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2023-05-14 18:22:20 +0200

    heuristic built for one-by-one box solving

--- g5/src/main/java/dtu/aimas/search/solvers/heuristics/SingleGoalDistanceCost.java
+++ g5/src/main/java/dtu/aimas/search/solvers/heuristics/SingleGoalDistanceCost.java
@@ -6,27 +6,40 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
+import dtu.aimas.common.Box;
 import dtu.aimas.common.Position;
 import dtu.aimas.communication.IO;
 import dtu.aimas.search.Problem;
 import dtu.aimas.search.solvers.graphsearch.State;
 import dtu.aimas.search.solvers.graphsearch.StateSpace;
 
-public class MixedDistanceSumCost implements Cost {
+public class SingleGoalDistanceCost implements Cost {
     @Override
     public int calculate(State state, StateSpace space) {
         var problem = space.problem();
         var result = 0;
 
         for(var goal : problem.boxGoals){
-            var minGoalDistance = state.boxes.stream()
-                    .filter(b -> b.label == goal.label)
-                    .map(b -> problem.admissibleDist(b.pos, goal.destination))
-                    .min(Integer::compareTo)
-                    .orElse(0);
-            //focus on keeping boxes in goals
-            if(minGoalDistance > 0) minGoalDistance+=1;
-            result += minGoalDistance;
+            if(state.boxes.stream().anyMatch(b -> b.label == goal.label && b.pos.equals(goal.destination)))
+                continue;
+            var compatibleBoxes = problem.boxes.stream().filter(b -> b.label == goal.label).toList();
+            var closestBox = compatibleBoxes.get(0);
+            var minDist = Integer.MAX_VALUE;
+            for(Box box : compatibleBoxes) {
+                var dist = problem.admissibleDist(box.pos, goal.destination);
+                if(dist < minDist) {
+                    closestBox = box;
+                    minDist = dist;
+                }
+            }
+            result += minDist;
+            if(minDist > 0) {
+                //go towards the box if it's out of its goal
+                Box b = closestBox;
+                var agent = problem.agents.stream().filter(a -> a.color == b.color).findFirst().get();
+                result += problem.admissibleDist(agent.pos, closestBox.pos)+1;
+            }
+            
         }
         //if(result < 2)IO.info(result);
         //if(result > 0) result += 10; //penalty to keep agent from going to his goal
